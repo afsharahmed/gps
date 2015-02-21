dba180b0a23b72f2ff325d131e04af1f36172771
Afshar <afshartech@gmail.com>
2015-02-22 01:00:33 +0530

    Added JUnit jars and added 'test' package to classpath

---
+++ test/com/gatepass/controller/VisitorControllerTest.java
@@ -0,0 +1,69 @@
+package com.gatepass.controller;
+
+import static org.junit.Assert.fail;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Afshar
+ *
+ */
+public class VisitorControllerTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link com.gatepass.controller.VisitorController#getDetails(java.lang.Integer)}.
+	 */
+	@Test
+	public void testGetDetails() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.gatepass.controller.VisitorController#printDetails(javax.servlet.http.HttpServletRequest)}.
+	 */
+	@Test
+	public void testPrintDetails() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.gatepass.controller.VisitorController#addVisitor(java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String, javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)}.
+	 */
+	@Test
+	public void testAddVisitor() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.gatepass.controller.VisitorController#updateVisitor(java.lang.Integer, java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String, java.lang.String, javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)}.
+	 */
+	@Test
+	public void testUpdateVisitor() {
+		fail("Not yet implemented");
+	}
+
+	/**
+	 * Test method for {@link com.gatepass.controller.VisitorController#deleteVisitor(java.lang.Integer, javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)}.
+	 */
+	@Test
+	public void testDeleteVisitor() {
+		fail("Not yet implemented");
+	}
+
+}
