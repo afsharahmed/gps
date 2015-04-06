c18724fe8c218909c429c7ef5b9e3ec2e08a5ac2
Afshar <afshartech@gmail.com>
2015-04-06 22:59:01 +0530

    Increased session timeout

--- src/com/gatepass/controller/VisitorController.java
+++ src/com/gatepass/controller/VisitorController.java
@@ -32,7 +32,7 @@ public final class VisitorController
 	
 	private VisitorService visitorService;
 
-
+	@Autowired
 	public void setVisitorService(VisitorService visitorService) {
 		this.visitorService = visitorService;
 	}
